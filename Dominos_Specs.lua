bb30465f229a49e5f20b3f0e5f3a7a95eff87fb8
Goranaws <lilspike_1861@yahoo.com>
2011-08-01 15:26:59 -0300

    Changed for simplicity, and to fix a minor error.

--- Dominos_Specs.lua
+++ Dominos_Specs.lua
@@ -1,28 +1,23 @@
-Swap = {}
-function Swap:OnEvent(event)
-	local spec = GetSpellTabInfo(GetPrimaryTalentTree()+1)
-	if event == "PLAYER_ENTERING_WORLD" then
-		if DominosSpecs == nil then
-			DominosSpecs = {}
-		end
-		local player = GetUnitName("player")
-		if not DominosSpecs[player] then
-			DominosSpecs[player] = {}
-		end
-		local s = DominosSpecs[player]
-
-
-		if not s[GetSpellTabInfo(GetPrimaryTalentTree()+1)] then
-			s[GetSpellTabInfo(GetPrimaryTalentTree()+1)] = 1
-			Dominos:SaveProfile(GetUnitName("player")..":"..spec)
-		end
-	elseif event == "PLAYER_TALENT_UPDATE" then
-		Dominos:SetProfile(  GetUnitName("player")..":"..spec)
+local function OnPlayerTalentUpdate(event)
+	if DominosSpecs == nil then
+		DominosSpecs = {};
 	end
+
+	local spec = GetSpellTabInfo(GetPrimaryTalentTree()+1);
+	local player = GetUnitName("player")
+
+	if not DominosSpecs[player] then
+		DominosSpecs[player] = {};
+	end
+
+	if not DominosSpecs[player][spec] then
+		Dominos:SaveProfile(player..":"..spec);
+		DominosSpecs[player][spec] = 1;
+	end
+
+	Dominos:SetProfile(player..":"..spec);
 end
 
-
-Swap.temp = CreateFrame("Frame")
-Swap.temp:RegisterEvent("PLAYER_TALENT_UPDATE")
-Swap.temp:RegisterEvent("PLAYER_ENTERING_WORLD")
-Swap.temp:SetScript("OnEvent", function(event) Swap:OnEvent(event) end)
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_TALENT_UPDATE")
+f:SetScript("OnEvent", function(event) OnPlayerTalentUpdate(event) end)
