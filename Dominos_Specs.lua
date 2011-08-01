33ca788aa685536a52a78459fcea62a545ef6e0e
Goranaws <lilspike_1861@yahoo.com>
2011-08-01 19:38:41 -0300

    Release version I think.

--- Dominos_Specs.lua
+++ Dominos_Specs.lua
@@ -1,23 +1,44 @@
-local function OnPlayerTalentUpdate(event)
-	if DominosSpecs == nil then
-		DominosSpecs = {};
+local f = CreateFrame("Frame")
+f.text = f:CreateFontString(nil, "BORDER", "GameFontNormal")
+f.l = 0
+local function OnPlayerTalentUpdate()
+	if f.l == 0 then
+		f.l = 1
+		--Whenever "PLAYER_TALENT_UPDATE" occurs, 
+		--it actually fires twice in a row, 
+		--so we want to skip it every other time.
+		--it also skips the very first uccurence which 
+		--fires off BEFORE "PLAYER_ENTERING_WORLD" and
+		--because it fires so early, would cause bad bugs.
+	elseif f.l == 1 then
+	
+		f.text:SetText(Dominos.db:GetCurrentProfile());
+		local current = f.text:GetText();
+		
+		if DominosSpecs == nil then
+			DominosSpecs = {};
+		end
+	
+		local player = GetUnitName("player");
+		local tree = select(2, GetTalentTabInfo(GetPrimaryTalentTree()));
+		local Ptree = player..":"..tree;
+	
+		if not DominosSpecs[player] then
+			DominosSpecs[player] = {};
+		end
+
+		if not DominosSpecs[player][tree] then
+			Dominos:SaveProfile(Ptree);
+			DominosSpecs[player][tree] = 1;
+		end
+	
+		if current ~= Ptree then
+			Dominos:SetProfile(Ptree);
+		end
+		f.l = 0
 	end
-
-	local spec = GetSpellTabInfo(GetPrimaryTalentTree()+1);
-	local player = GetUnitName("player")
-
-	if not DominosSpecs[player] then
-		DominosSpecs[player] = {};
-	end
-
-	if not DominosSpecs[player][spec] then
-		Dominos:SaveProfile(player..":"..spec);
-		DominosSpecs[player][spec] = 1;
-	end
-
-	Dominos:SetProfile(player..":"..spec);
 end
 
-local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_TALENT_UPDATE")
-f:SetScript("OnEvent", function(event) OnPlayerTalentUpdate(event) end)
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", function(event) OnPlayerTalentUpdate() end)
