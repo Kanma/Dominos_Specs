e10d6d8a666035a5492525ee4f67dd8ed8fb2298
Goranaws <lilspike_1861@yahoo.com>
2011-08-04 14:15:47 -0300

    Github for mac adds strange stuff sometimes...

--- Dominos_Specs.lua
+++ Dominos_Specs.lua
@@ -11,19 +11,6 @@ function Dominos_Specs:OnLoad()
 	end
 	
 	Dominos_Specs.player = DominosSpecs[player]
-<<<<<<< HEAD
-
-
-
-	local toggle = Dominos_Specs:AddToggleToDominosOptions()
-	toggle:Show()
-	
-	local f = CreateFrame('Frame')
-	f:RegisterEvent('PLAYER_TALENT_UPDATE')
-	f.text = f:CreateFontString(nil, 'BORDER', 'GameFontNormal')
-	
-	Dominos_Specs.ProfileManager = f
-=======
 	
 	Dominos_Specs.Toggle = Dominos_Specs:AddToggleToDominosOptions()
 	
@@ -35,7 +22,6 @@ function Dominos_Specs:OnLoad()
 	
 	Dominos_Specs.ProfileManager = f
 	Dominos_Specs.SpecProfileManager = e
->>>>>>> Preparing for full release on Curse.com
 
 	Dominos_Specs.menu = {}
 	Dominos_Specs.menuItems = { 'None',}
@@ -44,12 +30,6 @@ function Dominos_Specs:OnLoad()
 		table.insert(Dominos_Specs.menuItems, k)
 	end
 	for i= 1, 3 do
-<<<<<<< HEAD
-		Dominos_Specs.menu[i] = Dominos_Specs:NewMenu(UIParent, GetSpellTabInfo(i+1), i)
-	end
-	
-	Dominos_Specs:Activate()		
-=======
 		Dominos_Specs.menu[i] = Dominos_Specs:NewMenu(GetSpellTabInfo(i+1), i)
 	end
 	Dominos_Specs.Specmenu = {}
@@ -73,19 +53,11 @@ function Dominos_Specs:OnSpecChanged()
 	if (current ~= spec) and (spec ~= 'None') then
 		Dominos:SetProfile(spec);
 	end
->>>>>>> Preparing for full release on Curse.com
 end
 
 function Dominos_Specs:OnPlayerTalentUpdate()
 	Dominos_Specs.ProfileManager.text:SetText(Dominos.db:GetCurrentProfile());
 	local current = Dominos_Specs.ProfileManager.text:GetText();
-<<<<<<< HEAD
-	local tree = select(2, GetTalentTabInfo(GetPrimaryTalentTree()));
-	local Ptree = player..':'..tree;
-	
-	if (current ~= Ptree) and (Ptree ~= 'None') then
-		Dominos:SetProfile(DominosSpecs[player][tree]);
-=======
 	local tree = DominosSpecs[player]["2"][GetPrimaryTalentTree()];
 	
 	if (current ~= tree) and (tree ~= 'None') then
@@ -101,7 +73,6 @@ function Dominos_Specs:TurnOn()
 		Dominos_Specs.Toggle.toggle:Hide()
 		Dominos_Specs.Toggle.subtoggle:Hide()
 
->>>>>>> Preparing for full release on Curse.com
 	end
 end
 
@@ -115,45 +86,6 @@ function Dominos_Specs:Activate()
 		Dominos_Specs.ProfileManager:SetScript('OnEvent', nil)
 		for i= 1, #Dominos_Specs.menu do
 			Dominos_Specs.menu[i]:Hide()
-<<<<<<< HEAD
-		end
-	end
-end
-
-function Dominos_Specs:Enable(enable)
-	Dominos_Specs.player.enable = enable or nil
-	Dominos_Specs:Activate()
-end
-
-function Dominos_Specs:AddToggleToDominosOptions()
-
-	local name = 'Enable Dominos Specs'
-	local toggle = Dominos.Options:NewSmallCheckButton(name)
-	toggle.tooltipText = 'Click to allow Dominos Specs to automatically change your Dominos Profile when you swap active talent trees.'
-	toggle:SetScript('OnClick', function(self) Dominos_Specs:Enable(self:GetChecked()) end)
-	toggle:SetScript('OnShow', function(self) self:SetChecked(Dominos_Specs.player.enable)  end)
-	toggle:SetPoint('CENTER', Dominos.Options)
-	toggle:SetPoint('TOP', _G['DominosOptionsShow Tooltips in Combat'], 'BOTTOM', -8, -10)
-	toggle:Hide()
-	return toggle
-end
-
-function Dominos_Specs:NewMenu(pfr, name, i)
-local parent = Dominos.Options
-	local name = name..' Profile'
-	local frame = Dominos.Options:NewDropdown(name)
-
-	local r = DominosSpecs[player]
-	if not r[name] then
-		r[name] = 'None'
-	end
-
-	local function OnClick(self)
-		UIDropDownMenu_SetSelectedID(frame, self:GetID())
-		frame.id =  UIDropDownMenu_GetValue(self:GetID())
-		r[name] = Dominos_Specs.menuItems[self:GetID()]
-		frame.text = r[name]
-=======
 		end
 	end
 end
@@ -255,7 +187,6 @@ local parent = Dominos.Options
 		frame.id =  UIDropDownMenu_GetValue(self:GetID())
 		r["1"][x] = Dominos_Specs.menuItems[self:GetID()]
 		frame.text = r["1"][x]
->>>>>>> Preparing for full release on Curse.com
 	end
  
 	local function initialize(self, level)
@@ -266,19 +197,6 @@ local parent = Dominos.Options
 			info.value = v
 			info.func = OnClick
 			UIDropDownMenu_AddButton(info, level)
-<<<<<<< HEAD
-			frame.text = r[i]
-		end
-	end
-	
-		frame:SetScript('OnShow', function(self)
-			UIDropDownMenu_SetWidth(self, 110)
-			UIDropDownMenu_Initialize(self, initialize)
-			UIDropDownMenu_SetSelectedValue(self, r[name] or 'NONE')
-		end)
-	
-
-=======
 		end
 	end
 	
@@ -339,7 +257,6 @@ local parent = Dominos.Options
 		UIDropDownMenu_SetSelectedValue(self, r["2"][i] or 'NONE')
 	end)
 	
->>>>>>> Preparing for full release on Curse.com
 	UIDropDownMenu_JustifyText(frame, 'LEFT')
 
 	local prev = Dominos_Specs.menuFrame
@@ -355,8 +272,4 @@ end
 
 Dominos_Specs.Loader = CreateFrame('Frame')
 Dominos_Specs.Loader:RegisterEvent('PLAYER_ENTERING_WORLD')
-<<<<<<< HEAD
 Dominos_Specs.Loader:SetScript('OnEvent', function(event) Dominos_Specs:OnLoad()end)
-=======
-Dominos_Specs.Loader:SetScript('OnEvent', function(event) Dominos_Specs:OnLoad()end)
->>>>>>> Preparing for full release on Curse.com
